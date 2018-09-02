1420d71b73aadc22736d7f3ad5088bb0966f6139
Rich Churcher <rich.churcher@gmail.com>
2018-09-03 10:08:12 +1200

    Solution comments

--- client/components/Pixel.jsx
+++ client/components/Pixel.jsx
@@ -1,6 +1,11 @@
 import React from 'react'
 
-const randomColour = () => `#${Math.floor(Math.random()*16777215).toString(16)}`
+// Create a random hex colour.
+// It's not important to understand _exactly_ how this works right now, since this challenge is
+// mostly just about setting and changing component state. We use it in the same way we might use
+// Knex: it accomplishes a task for us. Try playing with the function in the Node REPL to see it in
+// action.
+const randomColour = () => `#${Math.floor(Math.random() * 16777215).toString(16)}`
 
 const width = 20
 const height = width
@@ -14,8 +19,9 @@ export default class Pixel extends React.Component {
         width,
         height,
         backgroundColor: randomColour()
-      },
-      update: setInterval(this.changeColour, 1000)
+      }
+      // Uncomment this to play with changing the colours every `n` seconds
+      // update: setInterval(this.changeColour, 1000)
     }
   }
 
@@ -35,8 +41,9 @@ export default class Pixel extends React.Component {
     const { style } = this.state
     return (
       <div
+        onClick={() => this.changeColour()}
         onMouseEnter={() => this.changeColour()}
-        style={style}></div>
+        style={style} />
     )
   }
 }
