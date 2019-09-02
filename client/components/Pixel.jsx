73ac92644c144aa4b6392ad70ce013ec97829a99
noemie-wandering-anemone <43393997+noemie-wandering-anemone@users.noreply.github.com>
2019-09-03 10:51:39 +1200

    Get style from component constructor

--- client/components/Pixel.jsx
+++ client/components/Pixel.jsx
@@ -1,11 +1,20 @@
 import React from 'react'
 
-const Pixel = props => {
-    return <div style={{
-        height: 20,
-        width: 20,
-        backgroundColor: 'purple',
-      }}></div>
+class Pixel extends React.Component {
+    constructor (props) {
+        super(props),
+        this.state = {
+            style: {
+                height: 20,
+                width: 20,
+                backgroundColor: 'purple',
+            }   
+        }
+    }
+
+    render() {
+        return <div style={this.state.style}></div>
+    }
 }
 
 export default Pixel
