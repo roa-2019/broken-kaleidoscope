061ecdf89f5c68d2409bcb3a1011bc580d921ff9
mariawenrose <mariarose.wen@gmail.com>
2019-09-03 11:43:09 +1200

    screed loopy loop

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,8 +1,17 @@
 import React from 'react'
+import Pixel from './Pixel'
 
 const App = () => {
+
+const screed = []
+for (let i = 0; i < 7000; i++) {
+  screed.push(<Pixel />)
+}
   return (
-    <div>React development has begun!</div>
+    <div>
+      <Pixel />
+      {screed}
+    </div>
   )
 }
 
---
+++ client/components/Pixel.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+// class App extends React.Component {
+//   render() {
+//   return <div></div>
+//   }
+// }
+
+var divStyle = {
+  backgroundColor: '#bfa2c8',
+  height: '100px',
+  width: '100px'
+}
+
+export default function ReturnDivs () {
+  return <div style = {divStyle}></div>
+}
+
+//make the loop that returns divs
+
+
+
