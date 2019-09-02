cb4bbbc2386abb486d9094ebb74eb475e2542830
Joshua Vial <joshua@enspiral.com>
2019-09-03 10:56:31 +1200

    remove warning for no key

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -3,24 +3,7 @@ import React from 'react'
 import Pixel from './Pixel'
 
 const App = () => {
-  // You could just as easily use traditional `for` loop syntax here:
-  //
-  //   const components = []
-  //   for (let i = 0; i < 10000; i++) {
-  //     components.push(<Pixel />)
-  //   }
-  //   return components
-  //
-  // Also notice that recent versions of React let you return an array from your component's render
-  // function! You could even write (note the commas):
-  //
-  //   return [
-  //     <Pixel />,
-  //     <Pixel />,
-  //     <Pixel />
-  //   ]
-  //
-  return Array.from({ length: 10000 }, () => <Pixel />)
+  return Array.from({ length: 10000 }, (item,i) => <Pixel key={i}/>)
 }
 
 export default App
