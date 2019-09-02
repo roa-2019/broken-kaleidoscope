87b420f75884be907c15549a029bdbdd347005c7
Will Houghton <48467321+WillHoughton@users.noreply.github.com>
2019-09-03 10:53:36 +1200

    made random colors appear

--- client/components/Pixel.jsx
+++ client/components/Pixel.jsx
@@ -1,27 +1,28 @@
 import React from 'react';
 
-
-
-
+const randomHexColor = () => 
+  `#${Math.floor(Math.random() * 0x1000000)
+    .toString(16)
+    .padStart(6, 0)}`
 
 class Pixel extends React.Component {
   constructor(props) {
     super(props);
 
     this.state = {
-    style: {
-    height: '100px',
-    width: '100px',
-    backgroundColor: 'blue'
-        }
-    }
+      style: {
+        height: '100px',
+        width: '100px',
+        backgroundColor: randomHexColor()
+      }
+    };
   }
 
+  
+
   render() {
-    return (
-      <div id='app' style={this.state.style}></div>
-    );
+    return <div id='app' style={this.state.style}></div>
   }
 }
 
-export default Pixel;
+export default Pixel
