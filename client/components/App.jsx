5a910b3fe82c9b9125b2cc58ab503c480d27ca35
Student <student@pop-os.localdomain>
2019-09-03 10:29:28 +1200

    create class based components for App and Pixel

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,9 +1,40 @@
 import React from 'react'
 
-const App = () => {
-  return (
-    <div>React development has begun!</div>
-  )
+
+
+class App extends React.Component {
+  constructor(props) {
+    super(props)
+  }
+  render() {
+    return (
+      <React.Fragment>
+        <div>React development has begun!</div>
+        <Pixel />
+      </React.Fragment>
+    )
+  }
 }
+
+
+class Pixel extends React.Component {
+  constructor(props) {
+    super(props)
+  }
+  render() {
+    return (
+      <React.Fragment>
+        <div></div>
+        <div style={{
+          height: 20,
+          width: 20,
+          backgroundColor: cornflowerblue
+        }} />
+      </React.Fragment>
+    )
+  }
+}
+
+
 
 export default App
