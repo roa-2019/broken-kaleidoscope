42d970b6cbeefc51adce80972eebf6b89fc959f5
mariawenrose <mariarose.wen@gmail.com>
2019-09-03 17:28:46 +1200

    one working test

--- tests/app.test.js
+++ tests/app.test.js
@@ -2,14 +2,24 @@ import React from 'react'
 import {shallow} from 'enzyme'
 
 import App from '../client/components/App'
+import Pixel from '../client/components/Pixel'
 
-test('<App />', () => {
+import { isTSAnyKeyword } from '@babel/types';
+
+test.skip('<App />', () => {
   const expected = 'React development has begun!'
   const wrapper = shallow(<App />)
   expect(wrapper.text()).toBe(expected)
 })
 
-test('<App />', () => {
-  const expected = 7000
-  const wrapper = shallow(<App />)
-})
+describe('<Pixel />', () => {
+  it('renders 7000 <Pixel Components />', () => {
+    const wrapper = shallow(<App />)
+  
+    console.log(wrapper.text)
+    let pixel = wrapper.find("Pixel" ).length
+
+
+      expect(pixel).toBe(7000)
+  })
+});
