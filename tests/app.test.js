657d8a34909cf1e9292980082c2f286ce7e90b6e
rosedhlakama <rosejembe@live.com>
2019-09-03 16:38:16 +1200

    added tests for Pixel componenet

--- tests/app.test.js
+++ tests/app.test.js
@@ -1,10 +1,30 @@
 import React from 'react'
 import {shallow} from 'enzyme'
 
-import App from '../client/components/App'
+import Pixel from '../client/components/Pixel'
 
-test('<App />', () => {
-  const expected = 'React development has begun!'
-  const wrapper = shallow(<App />)
-  expect(wrapper.text()).toBe(expected)
+describe('<Pixel />', () => {
+  test('each pixel is 20px wide / high', () => {
+
+    const expected = '20px'
+    const wrapper = shallow(<Pixel />)
+
+    let div = wrapper.find('div')
+
+    expect(div.prop('style').width).toBe(expected)
+    expect(div.prop('style').height).toBe(expected)
+
+  })
+
+
+  test('color changes when pixel is pressed', () => {
+
+    const wrapper = shallow(<Pixel />)
+    let div = wrapper.find('div')
+    let oldColor = div.prop('style').backgroundColor
+    div.simulate('click')
+    div = wrapper.find('div')
+    let newColor = div.prop('style').backgroundColor
+    expect(oldColor).not.toBe(newColor)
+  })
 })
