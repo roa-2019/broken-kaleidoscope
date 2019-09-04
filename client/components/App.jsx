e552b3099e1f4bb70bab34b6e22214265e1f4b0d
Alex-Abernethy <Alexander.Abernethy@gmail.com>
2019-09-04 12:14:29 +1200

    now with kanye quotes for some reason

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,11 +1,40 @@
 import React from 'react'
+import request from 'superagent'
 
 import Pixel from './Pixel'
 
-const App = () => {
-  return <div>
-    {Array.from({length: 10}, (pixel, i) => <Pixel key={i}/>)}
-  </div>
+class App extends React.Component {
+  constructor (props) {
+    super(props)
+    this.state = {
+      dumbQuote: ''
+    }
+    this.getDumbQuote = this.getDumbQuote.bind(this)
+  }
+
+  getDumbQuote() {
+    request.get('https://api.kanye.rest')
+      .then(result => {
+        this.setState({
+          dumbQuote: result.body.quote
+        })
+      })
+  }
+
+render () {
+    return <div id="app">
+      <h1>Now with Kanye quotes for some reason</h1>
+      <button onClick={this.getDumbQuote}>Why would you click this</button>
+      <h2>{this.state.dumbQuote}</h2>
+      {Array.from({length: 10}, (pixel, i) => <Pixel key={i}/>)}
+    </div>
+  }
 }
+
+// const App = () => {
+//   return <div>
+//     {Array.from({length: 10}, (pixel, i) => <Pixel key={i}/>)}
+//   </div>
+// }
 
 export default App
