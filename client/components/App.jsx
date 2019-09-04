5121586c75dc46cf31b66160fd066680bc962000
Student <student@pop-os.localdomain>
2019-09-04 12:39:12 +1200

    API working

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,9 +1,35 @@
 import React from 'react'
+import request from 'superagent'
 
-const App = () => {
-  return (
-    <div>React development has begun!</div>
-  )
+
+class App extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      details:''
+    }
+    this.getBeer = this.getBeer.bind(this)
+  }
+
+  getBeer() {
+    request.get('https://api.punkapi.com/v2/beers/random')
+      .then(result => {
+        console.log(result.body[0])
+        this.setState({
+          details: result.body
+        })
+      })
+  }
+
+  render() {
+    return (
+      <React.Fragment>
+        <div>BrewDog!</div>
+        <button onClick={this.getBeer}>Click for a random BrewDog Beer!</button>
+        <p></p>
+      </React.Fragment>
+    )
+  }
 }
 
 export default App
