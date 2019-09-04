01a6805c2ed649296b9459dde8fc3b0ec81879c6
Student <student@pop-os.localdomain>
2019-09-04 15:40:00 +1200

    kanye api not working yet

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,5 +1,7 @@
 import React from 'react'
-import Pixel from './Pixel'
+// import Pixel from './Pixel'
+import request from 'superagent'
+
 
 // const App = () => {
 //   return (
@@ -11,86 +13,49 @@ import Pixel from './Pixel'
 class App extends React.Component {
   constructor(props){
     super(props)   
+    this.state = {
+      quote:  "i'm a quote"
+    }
+    this.getQuote=this.getQuote.bind(this)
   }
+  getQuote () {
+    request.get('https://api.kanye.rest/')
+    .then(result => {
+    this.ListeningStateChangedEvent({
+      quote: result.body.quote
+    })
+  })
+  }
+    toggleVis() {
+    this.setState ({
+      isVisible: this.state.isVisible
 
-    render(){
+    })
+  }
+    render() {
       return(
         <React.Fragment>
-         
+          <div> 
+        {this.state.isVisible && <h1> {this.state.quote}</h1>}
+        <button onClick = {this.getQuote}> Click Me!</button>
+        <button onClick={this.getQuote}>Click Me!</button>
+        </div>
+
+
+
+
+          {/* <Pixel />
           <Pixel />
           <Pixel />
           <Pixel />
           <Pixel />
           <Pixel />
-          <Pixel />
-          <Pixel />
-          <Pixel />
-          <Pixel />
-          <Pixel />
-          <Pixel />
-          <Pixel />
-          <Pixel />
-          <Pixel />
-          <Pixel />
-          <Pixel />
-          <Pixel />
-          <Pixel />
-          <Pixel />
-          <Pixel />
-          <Pixel />
-          <Pixel />
-          <Pixel />
-          <Pixel />
-          <Pixel />
-          <Pixel />
-          <Pixel />
-          <Pixel />
-          <Pixel />
-          <Pixel />
-          <Pixel />
-          <Pixel />
-          <Pixel />
-          <Pixel />
-          <Pixel />
-          <Pixel />
-          <Pixel />
-          <Pixel />
-          <Pixel />
-          <Pixel />
-          <Pixel />
-          <Pixel />
-          <Pixel />
-          <Pixel />
-          <Pixel />
-          <Pixel />
-          <Pixel />
-          <Pixel />
-          <Pixel />
-          <Pixel />
-          <Pixel />
-          <Pixel />
-          <Pixel />
-          <Pixel />
-          <Pixel />
-          <Pixel />
-          <Pixel />
-          <Pixel />
-          <Pixel />
-          <Pixel />
-          <Pixel />
-          <Pixel />
-          <Pixel />
-          <Pixel />
-          <Pixel />
-          <Pixel />
-          <Pixel />
-          <Pixel />
-          <Pixel />
+          <Pixel /> */}
+
+     
         </React.Fragment>
       )
     }
   }
-
-
 
 export default App
